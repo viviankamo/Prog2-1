34bc656dba8d5b2f575c2d2c4bc0327e65cc721c
viviankamo <vivian.kamo@elev.ga.ntig.se>
2020-09-14 20:18:40 +0200

    project-array-names

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,82 +8,25 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello Niklas!");
 
-            
-
-            int i = 0;
-            while (i < 1)
+            string[] names = { "vivian", "filippa", "Philip", "amanda", "mary" };
+            for (int i = 0; i < names.Length; i++)
             {
+                names[i] = i;
                 Console.WriteLine(i);
-                i++;
-
-
-                Console.WriteLine("How old are you?");
-
-                int myAge = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Your age is: " + myAge);
-
             }
-
-            Console.WriteLine("What is your name?");
-
-            string userName = Console.ReadLine();
-
-            Console.WriteLine("Your name is: " + userName);
-
-            Console.WriteLine("Are you alive?");
-
-            bool Lifetrue = true;
-
-            string answer = Console.ReadLine();
-
-            if (answer == "yes")
+            Console.WriteLine("........................");
+            
+            int sum = 0;
+            for (int i = names.Length -1; i >= 0; i--)
             {
-                Console.WriteLine("You are alive");   // Outputs True
-            }
-
-            else if (answer == "no")
-            {
-                Console.WriteLine("You are dead");   // Outputs False
-
+                sum += names[];
+                Console.WriteLine(sum);
             }
 
 
-            Console.WriteLine("The best colour is:");
 
-            int color = 1;
-            switch (color) { 
-                case 1:
-                    Console.WriteLine("blue");
-                    break;
-                case 2:
-                    Console.WriteLine("green");
-                    break;
-                case 3:
-                    Console.WriteLine("red");
-                    break;
-                case 4:
-                    Console.WriteLine("yellow");
-                    break;
-            }
-            // Outputs "blue" (color 1)
-
-
-            for (int v = 0; v < 5; v++)
-            {
-                Console.WriteLine(v);
-                v++;
-
-
-                Console.WriteLine("You are done, Mate!");
-
-            }
         }
 
 
-
-
-
-
-}
+        }
     }
---
+++ HelloWorld/projekt2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace projekt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] names = new string[5];
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            Console.WriteLine("........................");
+
+            
+            for (int i = names.Length - 1; i >= 0; i--)
+            {
+                
+                Console.WriteLine(names[i]);
+            }
+
+
+
+        }
+
+
+    }
+
+    }
+
