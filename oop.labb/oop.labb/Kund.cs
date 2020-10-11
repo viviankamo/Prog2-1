7c5de9a814c4c313074899e7cc0657dd47f1b35a
viviankamo <vivian.kamo@elev.ga.ntig.se>
2020-10-11 22:49:05 +0200

    oop

---
+++ oop.labb/oop.labb/Kund.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop.labb
+{
+    class Kund
+    {
+        public List<Produkt> kundkorg = new List<Produkt>();
+    }
+}
---
+++ oop.labb/oop.labb/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace oop.labb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kund = new Kund();
+            var produkt = new Produkt();
+            produkt.produktTyp = "gris";
+            kund.kundkorg.Add(produkt);
+
+            foreach(var prod in kund.kundkorg){
+                Console.WriteLine(prod.produktTyp);
+            }
+        }
+    }
+}
